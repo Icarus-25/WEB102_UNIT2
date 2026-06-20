961bc20bdfc3d6b2973c33d4bd7d3443b9a35e32
Joel Echeverri <s-joel.echeverrimart@lwtech.edu>
2026-06-19 19:51:20 -0700

    Create the react basic setup

---
+++ country-capitals/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import { useState } from 'react';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>Capital of America</h1>
+        <h2>Can you guess all capitals of the countries in America?</h2>
+        <H2>Count:</H2>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
