1ab95a54c9aa3b6c4d4e693d86932b4053999e97
Joel Echeverri <s-joel.echeverrimart@lwtech.edu>
2026-06-19 22:04:49 -0700

    create useState variables for the card index and flip

--- country-capitals/src/App.jsx
+++ country-capitals/src/App.jsx
@@ -1,15 +1,44 @@
 import './App.css';
 import { useState } from 'react';
 import { americaCards } from './data/americaCards';
+import Flashcard from './components/Flashcard';  
 
-const App = () => {
+const App = () => { 
+const [currentIndex, setCurrentIndex] = useState(Math.floor(Math.random() * americaCards.length))
+const [flipped, setFlipped] = useState(false)
 
+const currentCard = americaCards[currentIndex] 
+
+const getRandomCard = () => {
+    let newIndex;
+    do {
+      newIndex = Math.floor(Math.random() * americaCards.length);
+    } while (newIndex === currentIndex);  
+
+    setCurrentIndex(newIndex);
+    setFlipped(false);
+  };
+
+  
   return (
     <div className="App">
       <div className="header">
         <h1>Capital of America</h1>
         <h2>Can you guess all capitals of the countries in America?</h2>
-        <H2>Count:</H2>
+        <h2>Count: {americaCards.length} </h2>
+      </div>
+
+      <Flashcard 
+        card={currentCard} 
+        flipped={flipped}
+        setFlipped={setFlipped}
+        onNext={getRandomCard}
+      />
+
+      <div className="controls">
+        <button onClick={getRandomCard} className="next-btn">
+          Next Random Country →
+        </button>
       </div>
       
     </div>
