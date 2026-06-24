c7c5ee588b8b3a9f72e39910633e9841c4c1e558
Joel Echeverri <s-joel.echeverrimart@lwtech.edu>
2026-06-23 19:41:21 -0700

    refactor: simplify Flashcard component

--- country-capitals/src/components/Flashcard.jsx
+++ country-capitals/src/components/Flashcard.jsx
@@ -14,30 +14,22 @@ const Flashcard = ({ card, flipped, setFlipped }) => {
         onClick={handleFlip}
       >
         <div className="flashcard-inner">
-          
-          {/* FRONT - Country + Flag */}
           <div className="flashcard-front">
             <div className="card-content">
               <div className="flag-wrapper">
-                <img 
-                  src={flagUrl} 
-                  alt={`${card.country} flag`} 
-                  className="country-flag" 
-                />
+                <img src={flagUrl} alt={`${card.country} flag`} className="country-flag" />
               </div>
               <p className="label">COUNTRY</p>
               <h2>{card.country}</h2>
             </div>
           </div>
 
-          {/* BACK - Capital */}
           <div className="flashcard-back">
             <div className="card-content">
               <p className="label">CAPITAL CITY</p>
               <h2>{card.capital}</h2>
             </div>
           </div>
-
         </div>
       </div>
     </div>
